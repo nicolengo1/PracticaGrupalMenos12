b368549c1d55045456f213c2f082e7e0f1403294
nicolengo1 <nicomc999@gmail.com>
2025-12-18 22:56:05 +0100

    asd

--- turejuveneces.pas
+++ turejuveneces.pas
@@ -1,51 +1,140 @@
-program turejuveneces;
-
-uses UnidadClinica;
-
-var 
-opcion := integer;
-
-
-begin
-
-randomize();
-
- repeat
-
-    menu();
-
-    readln(opcion);
-
-    if (opcion <> 7) then
-    begin
-    case  of opcion
-    1:if (arpaciente.tope >= MAX_ARRAY) then
-       writeln('No se pueden agregar mas pacientes, capacidad maxima alcanzada !')
-      else
-      begin
-
-
-        AddPaciente()//ArrPaciente y paciente
-      end;
-        
-
-    2:
-    3:
-    4:
-    5:
-    6:
-    else
-    writeln('Opcion incorrecta!');
-        
-    end;
-
-    else
-    begin
-     writeln('byebyeee');
-      // procedure guardar
-    end;
-    
- until (opcion = 7);
-
-end.
-
+<?xml version="1.0" encoding="UTF-8"?>
+<CONFIG>
+  <ProjectSession>
+    <PathDelim Value="\"/>
+    <Version Value="12"/>
+    <BuildModes Active="Default"/>
+    <Units>
+      <Unit>
+        <Filename Value="project1.lpr"/>
+        <IsPartOfProject Value="True"/>
+        <UnitName Value="turejuveneces"/>
+        <TopLine Value="39"/>
+        <CursorPos X="124" Y="81"/>
+        <UsageCount Value="31"/>
+        <Loaded Value="True"/>
+      </Unit>
+      <Unit>
+        <Filename Value="unidadclinica.pas"/>
+        <IsPartOfProject Value="True"/>
+        <UnitName Value="UnidadClinica"/>
+        <IsVisibleTab Value="True"/>
+        <EditorIndex Value="1"/>
+        <TopLine Value="116"/>
+        <CursorPos X="35" Y="140"/>
+        <UsageCount Value="31"/>
+        <Loaded Value="True"/>
+      </Unit>
+    </Units>
+    <JumpHistory HistoryIndex="25">
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="88" Column="47" TopLine="73"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="92" Column="38" TopLine="74"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="91" Column="20" TopLine="75"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="90" Column="21" TopLine="75"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="96" Column="13" TopLine="80"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="101" TopLine="81"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="102" Column="9" TopLine="81"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="104" Column="5" TopLine="82"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="105" Column="13" TopLine="85"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="40" Column="20" TopLine="34"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="105" Column="26" TopLine="90"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="133" Column="18" TopLine="122"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="136" Column="44" TopLine="113"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="68" Column="81" TopLine="62"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="192" Column="40" TopLine="176"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="229" Column="26" TopLine="215"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="355" Column="7" TopLine="335"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="362" Column="26" TopLine="345"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="392" TopLine="377"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="190" Column="20" TopLine="167"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="44" Column="11" TopLine="29"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="152" Column="114" TopLine="19"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="44" Column="11" TopLine="28"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="46" TopLine="30"/>
+      </Position>
+      <Position>
+        <Filename Value="unidadclinica.pas"/>
+        <Caret Line="192" Column="5" TopLine="167"/>
+      </Position>
+      <Position>
+        <Filename Value="project1.lpr"/>
+        <Caret Line="114" Column="12" TopLine="91"/>
+      </Position>
+    </JumpHistory>
+    <RunParams>
+      <FormatVersion Value="2"/>
+      <Modes ActiveMode=""/>
+    </RunParams>
+  </ProjectSession>
+</CONFIG>
